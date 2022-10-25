b38a562838661cf824f5c7eb6a404d918c46526d
40754 <40754@LAPTOP-QGKSDHS9>
2022-10-25 22:40:43 +0200

    Add project files.

---
+++ Sugaru_Iulia_Lab2/Data/Sugaru_Iulia_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sugaru_Iulia_Lab2.Models;
+
+namespace Sugaru_Iulia_Lab2.Data
+{
+    public class Sugaru_Iulia_Lab2Context : DbContext
+    {
+        public Sugaru_Iulia_Lab2Context (DbContextOptions<Sugaru_Iulia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sugaru_Iulia_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Sugaru_Iulia_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Sugaru_Iulia_Lab2.Models.Author> Author { get; set; }
+    }
+}
---
+++ Sugaru_Iulia_Lab2/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sugaru_Iulia_Lab2.Data;
+using Sugaru_Iulia_Lab2.Models;
+
+namespace Sugaru_Iulia_Lab2.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Sugaru_Iulia_Lab2.Data.Sugaru_Iulia_Lab2Context _context;
+
+        public DetailsModel(Sugaru_Iulia_Lab2.Data.Sugaru_Iulia_Lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
