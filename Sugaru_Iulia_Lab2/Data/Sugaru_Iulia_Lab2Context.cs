585736637bb5819adb3acb367349d6f10ef27856
iuliasugaru <iulia.sugaru@yahoo.com>
2022-11-15 17:36:31 +0100

    lab5 BUN (fara task ul final)

--- Sugaru_Iulia_Lab2/Data/Sugaru_Iulia_Lab2Context.cs
+++ Sugaru_Iulia_Lab2/Data/Sugaru_Iulia_Lab2Context.cs
@@ -21,5 +21,8 @@ namespace Sugaru_Iulia_Lab2.Data
         public DbSet<Sugaru_Iulia_Lab2.Models.Author> Author { get; set; }
 
         public DbSet<Sugaru_Iulia_Lab2.Models.Category> Category { get; set; }
+        public DbSet<Sugaru_Iulia_Lab2.Models.Member> Member { get; set; }
+        public DbSet<Sugaru_Iulia_Lab2.Models.Borrowing>Borrowing { get; set; }
+
     }
 }
--- Sugaru_Iulia_Lab2/Migrations/20221115153142_Borrowings.Designer.cs
+++ Sugaru_Iulia_Lab2/Migrations/20221115153142_Borrowings.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sugaru_Iulia_Lab2.Data;
 
@@ -11,9 +12,10 @@ using Sugaru_Iulia_Lab2.Data;
 namespace Sugaru_Iulia_Lab2.Migrations
 {
     [DbContext(typeof(Sugaru_Iulia_Lab2Context))]
-    partial class Sugaru_Iulia_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221115153142_Borrowings")]
+    partial class Borrowings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
